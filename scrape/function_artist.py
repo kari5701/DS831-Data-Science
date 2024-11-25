c42a858011bea571d536d29405068893434f56a7
geouck <eskealler@outlook.com>
2024-11-25 12:45:05 +0100

    Re add cell names
    Lyricists and composers

--- scrape/function_artist.py
+++ scrape/function_artist.py
@@ -15,7 +15,9 @@ def extract_song_details(url):
         "Length": "",
         "Label": "",
         "Songwriters": "",
-        "Producers": ""
+        "Producers": "",
+        "Lyricist(s)": "",
+        "Composer(s)": ""
     }
 
     # Title from the page header
@@ -35,16 +37,20 @@ def extract_song_details(url):
                 if "Released" in header_text:
                     song_details["Release Date"] = value.text.strip()
                 elif "Genre" in header_text:
-                    song_details["Genres"] = ', '.join(part.strip() for part in value.get_text(separator=',').split(',') if part.strip())
+                    song_details["Genres"] = value.text.strip()
                 elif "Length" in header_text:
                     song_details["Length"] = value.text.strip()
                 elif "Label" in header_text:
-                    song_details["Label"] = value.get_text(separator=', ').strip()
+                    song_details["Label"] = value.text.strip()
                 elif "Songwriter" in header_text:
-                    song_details["Songwriters"] = ', '.join(part.strip() for part in value.get_text(separator=',').split(',') if part.strip())
+                    song_details["Songwriters"] = value.text.strip()
                 elif "Producer" in header_text:
-                    song_details["Producers"] = ', '.join(part.strip() for part in value.get_text(separator=',').split(',') if part.strip())
-
+                    song_details["Producers"] = value.text.strip()
+                elif "Producer" in header_text:
+                    song_details["Lyricist(s)"] = value.text.strip()               
+                elif "Producer" in header_text:
+                    song_details["Composer(s)"] = value.text.strip()
+                    
             # Look for artist information in the header with keywords indicating it's a single by certain artists
             if header and 'description' in header.get('class', []) and "Single by" in header.text:
                 # Only get the names from <a> tags within this header, excluding "Single"
