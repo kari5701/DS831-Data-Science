193c285f14d5876ebcab2f8bb74f073afc9cedb8
geouck <eskealler@outlook.com>
2024-11-27 09:28:46 +0100

    Forskellige ting!

--- html_scrape.py
+++ html_scrape.py
@@ -1,26 +1,26 @@
 import os
 import pandas as pd
-
 from function_artist import extract_song_details_from_file
 
 # HTML Directory
 # Test test test test
-directory_path = 'billboard_articles'
+directory_path = os.path.normpath('billboard_articles')
 
 # Collect all song details
 all_song_details = []
 for filename in os.listdir(directory_path):
     if filename.endswith('.html'):
-        filepath = os.path.join(directory_path, filename)
+        filepath = os.path.normpath(os.path.join(directory_path, filename))
         details = extract_song_details_from_file(filepath)
         all_song_details.append(details)
         print(f"Extracted data for: {filename}")
 
 # Save results to a CSV file
-csv_name = "html_scrape"
+csv_name = "html_scrape_edited_artist_function"
+
+csv_path = os.path.normpath(os.path.join('data', f'{csv_name}.csv'))
 
 df = pd.DataFrame(all_song_details)
-df.to_csv(f'{csv_name}.csv', index=False)
-print(f"{len(df)}Datapoints has been saved to {csv_name}.csv")
+df.to_csv(os.path.normpath(f'{csv_path}.csv'), index=False)
 
-from datacleanfunctions import remove_citations, remove_citationswcommas, remove_extra_commas
+print(f"{len(df)} rows of info has been saved to {csv_path}.csv")
